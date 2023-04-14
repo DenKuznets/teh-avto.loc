cf2f87fa8d3f6c32c3ddf2fa689656b3b5115904
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-14 13:34:54 +0300

    done

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,17 +3,16 @@
 @include("layouts.head")
 
 <body>
-    <div class="bg">
-        @include("layouts.header")
-        <button class="burger">
-            <img src="{{ asset('assets/img/burger.svg') }}" alt="">
-        </button>
-        <button class="mobile-menu__close-btn">
-            <img src="{{ asset('assets/img/close-btn.svg') }}" alt="">
-        </button>
-        @yield("content")
-        @include("layouts.footer")
-    </div>
+    @include("layouts.header")
+    <button class="burger">
+        <img src="{{ asset('assets/img/burger.svg') }}" alt="">
+    </button>
+    <button class="mobile-menu__close-btn">
+        <img src="{{ asset('assets/img/close-btn.svg') }}" alt="">
+    </button>
+    @yield("content")
+    @include("layouts.footer")
+
     <script src="{{ mix('/assets/js/main.js') }}"></script>
     @yield("scripts")
 </body>
